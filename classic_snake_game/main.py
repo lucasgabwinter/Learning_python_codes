7138b9da6a9838bc99906586d2889ff02baef69a
Lucas Gabriel Winter <102532062+lucasgabwinter@users.noreply.github.com>
2022-11-15 17:10:31 -0300

    Create main.py

---
+++ classic_snake_game/main.py
@@ -0,0 +1,43 @@
+from turtle import Screen
+import time
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+score = Scoreboard()
+snake = Snake()
+food = Food()
+screen.listen()
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+    screen.onkey(snake.up, "Up")
+    screen.onkey(snake.down, "Down")
+    screen.onkey(snake.left, "Left")
+    screen.onkey(snake.right, "Right")
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        score.add_score()
+        snake.extend()
+        food.refresh()
+    # Detect collision with screen corners
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() < -280 or snake.head.ycor() > 280:
+        game_is_on = False
+        score.game_over()
+    # Detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            score.game_over()
+
+
+screen.exitonclick()
